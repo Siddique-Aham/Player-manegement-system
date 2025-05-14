ad63cc0714308b377fb6adced77ee7c6a2d1a5d9
Siddique Aham <siddiqueaham05@gmail.com>
2025-05-14 13:23:07 +0530

    Create PlayerManager.h

---
+++ PlayerManager.h
@@ -0,0 +1,12 @@
+#ifndef PLAYERMANAGER_H
+#define PLAYERMANAGER_H
+
+void addPlayer();
+void displayAll();
+void searchPlayer();
+void removePlayer();
+void updatePlayer();
+void displayTop3();
+void menu();
+
+#endif
