490b09fccc897a117ea0c08af75c1078f661ec96
Siddique Aham <siddiqueaham05@gmail.com>
2025-05-14 13:23:46 +0530

    Create PlayerManager.cpp

---
+++ PlayerManager.cpp
@@ -0,0 +1,11 @@
+#include "Player.h"
+#include "PlayerManager.h"
+#include <iostream>
+using namespace std;
+
+// Global variables
+const int MAX = 12;
+Player players[MAX];
+int playerCount = 0;
+
+// Definitions of addPlayer(), displayAll(), etc.
